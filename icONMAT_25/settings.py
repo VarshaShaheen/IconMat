3e3325fa5aede586609b3a7af563f08867c9d51c
ajuroshan <ajuaju0483@gmail.com>
2024-09-24 19:29:20 +0000

    commit from prod server

--- icONMAT_25/settings.py
+++ icONMAT_25/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uu8gaclt@k1=ht(6474z_g6=&+14!2hzsui)&aq+r6yq*p!ks9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iconmat2025.cusat.ac.in', 'www.iconmat2025.cusat.ac.in']
 
 
 # Application definition
@@ -115,11 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Adjust this path if your static directory is elsewhere
+#STATICFILES_DIRS = [
+#	BASE_DIR / "static",
+#]
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
