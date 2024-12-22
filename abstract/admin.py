4100bebe7e7961c94b40a25cbecf690ee086a013
ajuroshan <ajuaju0483@gmail.com>
2024-12-22 16:12:17 +0530

    feat: added export feature

--- abstract/admin.py
+++ abstract/admin.py
@@ -1,11 +1,26 @@
+from .models import PaperAbstract
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportMixin
+
+
 
 # Register your models here.
 
-from .models import PaperAbstract
+
+class PaperResource(resources.ModelResource):
+
+	def dehydrate_abstract(self, paper):
+		return f"https://iconmat2025.cusat.ac.in/media/{paper.abstract}"
+	class Meta:
+		model = PaperAbstract
+
+
 
 @admin.register(PaperAbstract)
-class PaperAbstractAdmin(admin.ModelAdmin):
+class PaperAbstractAdmin(ExportMixin,admin.ModelAdmin):
 	list_display = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
 	list_filter = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
 	search_fields = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
+	resource_classes = [PaperResource]
+
