1a1ef446873544af0422841db9eb02477940b36a
ajuroshan <ajuaju0483@gmail.com>
2025-01-02 15:10:05 +0530

    feat: added user email to paper abstract admin

--- abstract/admin.py
+++ abstract/admin.py
@@ -19,8 +19,13 @@ class PaperResource(resources.ModelResource):
 
 @admin.register(PaperAbstract)
 class PaperAbstractAdmin(ExportMixin,admin.ModelAdmin):
-	list_display = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
+	list_display = ['title', 'name','phone_number','user_email','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
 	list_filter = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
 	search_fields = ['title', 'name','phone_number','title_of_abstract','designation','organization','symposia','abstract','mode_of_presentation']
 	resource_classes = [PaperResource]
+
+	def user_email(self, obj):
+		return obj.user.email if obj.user else "No User"
+
+	user_email.short_description = "User Email"
 
