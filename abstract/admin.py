020a1f961d4d6e992870f95ed77b1a390be219b6
ajuroshan <ajuaju0483@gmail.com>
2025-01-02 18:02:57 +0530

    fix: export email also

--- abstract/admin.py
+++ abstract/admin.py
@@ -2,6 +2,7 @@ from .models import PaperAbstract
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ExportMixin
+from import_export import resources, fields, widgets
 
 
 
@@ -10,10 +11,21 @@ from import_export.admin import ExportMixin
 
 class PaperResource(resources.ModelResource):
 
+	user_email = fields.Field(
+		attribute='user',
+		column_name='user_email',
+		widget=widgets.CharWidget()
+	)
 	def dehydrate_abstract(self, paper):
 		return f"https://iconmat2025.cusat.ac.in/media/{paper.abstract}"
+
+	def dehydrate_user_email(self, paper):
+		return paper.user.email if paper.user else "No User"
+
 	class Meta:
 		model = PaperAbstract
+		fields = ['title', 'name', 'phone_number', 'user_email', 'title_of_abstract',
+		          'designation', 'organization', 'symposia', 'abstract', 'mode_of_presentation']
 
 
 
