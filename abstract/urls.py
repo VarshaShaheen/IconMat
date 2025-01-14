1207a9801e0644a7d6ffd5010fd7d070973978ba
ajuroshan <ajuaju0483@gmail.com>
2025-01-14 17:13:14 +0530

    feat: full paper submission to prod

--- abstract/urls.py
+++ abstract/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('submit_abstract/', views.abstract_submission, name='abstract_submission'),
     path('submit_full_paper/', views.full_paper_submission, name='full_paper_submission'),
+    path('fullpaper_details/', views.FullPaperDetails.as_view(), name='full_paper_details'),
     path('abstract_details/', views.AbstractDetail.as_view(), name='abstract_details'),
     path('view-abstract/', views.view_abstract, name='view_abstract'),
 ]
--- abstract/views.py
+++ abstract/views.py
@@ -60,6 +60,16 @@ class AbstractDetail(TemplateView):
 		context['dates'] = Dates.objects.last()
 		return context
 
+
+class FullPaperDetails(TemplateView):
+	template_name = 'abstract/full_paper_details.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['dates'] = Dates.objects.last()
+		return context
+
+
 @login_required
 def view_abstract(request):
 	abstracts = PaperAbstract.objects.filter(user=request.user)
