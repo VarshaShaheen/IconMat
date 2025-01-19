9349b970502b37b0a2d3bfb23587fd54f2df9f5b
ajuroshan <ajuaju0483@gmail.com>
2025-01-19 13:54:52 +0530

    feat: added search to payment admin

--- payment/admin.py
+++ payment/admin.py
@@ -6,4 +6,4 @@ from payment.models import Payment
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['user__first_name', 'fee_structure', 'status_code', 'ref_no', 'date']
-    search_fields = ['user__first_name', 'ref_no']
+    search_fields = ['user', 'ref_no']
